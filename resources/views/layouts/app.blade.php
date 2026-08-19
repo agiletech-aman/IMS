b9af6c9e378ea5aecdbc7cd1470d7de517d3e0d3
buildwitharix <buildwitharix@gmail.com>
2026-08-19 12:15:34 +0530

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Dashboard') | {{ $systemSettings?->application_name ?? 'Agile Tech Solutions IIM' }}</title>
+    <link rel="icon" type="image/png" href="{{ \App\Support\PublicUrl::asset('static/img/agile-tech-logo.png') }}">
     <script>
         (() => {
             const savedTheme = 'light';
